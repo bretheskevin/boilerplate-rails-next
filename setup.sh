400ab0a85f21151933ff26b97f6e3304e0380877
Kévin Brèthes <bretheskevin@gmail.com>
2024-04-22 00:31:38 +0200

    Added rspec to precommit

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# look for "PROJECT_NAME="*" in the pre-commit file, and replace it with the project name from .env
+
+PROJECT_NAME=$(grep -oP '(?<=PROJECT_NAME=").*(?=")' .env)
+sed -i "s/PROJECT_NAME=\".*\"/PROJECT_NAME=\"$PROJECT_NAME\"/g" .dev/pre-commit
+
 cp .dev/pre-commit .git/hooks/pre-commit
